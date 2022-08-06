320bd6f6230fc95cef53aadac0c0141acc089276
athnf <70727135+athnf@users.noreply.github.com>
2022-08-06 22:15:59 +0700

    Add files via upload

---
+++ volume balok.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int p,l,t,v; // Deklarasi variabel panjang,lebar,tinggi,volume
+
+    cout<<"PROGRAM C++ MENGHTUNG VOLUME BALOK"<<endl;
+    cout<<"----------------------------------"<<endl;
+    //Menginput nilai panjang,lebar dan tinggi
+    cout<<"Masukan Panjang : ";
+    cin>>p;
+    cout<<"Masukan Lebar   : ";
+    cin>>l;
+    cout<<"Masukan Tinggi  : ";
+    cin>>t;
+    cout<<endl;
+
+    v=p*l*t; //Rumus untuk menghitung volume balok
+
+    cout<<"Volume Balok    = "<<v<<" "; //Tampilkan hasil
+    return 0;
+
+}
