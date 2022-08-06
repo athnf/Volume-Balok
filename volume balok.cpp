a7550a578d8f16c3ed1d6f359534d8e94b95c35f
athnf <70727135+athnf@users.noreply.github.com>
2022-08-06 22:13:57 +0700

    Update volume balok.cpp

--- volume balok.cpp
+++ volume balok.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 int main(){
     int p,l,t,v; // Deklarasi variabel panjang,lebar,tinggi,volume
-    string dimensi;
 
     cout<<"PROGRAM C++ MENGHTUNG VOLUME BALOK"<<endl;
     cout<<"----------------------------------"<<endl;
@@ -14,13 +13,11 @@ int main(){
     cin>>l;
     cout<<"Masukan Tinggi  : ";
     cin>>t;
-    cout<<"Masukan Satuan (mm,cm,m) Dimensi Balok : ";
-    cin>>dimensi;
     cout<<endl;
     
     v=p*l*t; //Rumus untuk menghitung volume balok
     
-    cout<<"Volume Balok    = "<<v<<" "<<dimensi<<"3"; //Tampilkan hasil
+    cout<<"Volume Balok    = "<<v<<" "<<"3"; //Tampilkan hasil
     return 0;
 
 }
